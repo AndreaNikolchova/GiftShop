ebc568ae483c1109509f24b97c23bc11f308ca86
AndreaNikolchova <a.nikolchova04@gmail.com>
2023-06-30 14:58:26 +0300

    First commit!

---
+++ GiftShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GiftShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
