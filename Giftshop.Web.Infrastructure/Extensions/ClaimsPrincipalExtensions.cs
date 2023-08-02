a0c7049101260aa21debd1cd8acd4d2f9de5897a
AndreaNikolchova <a.nikolchova04@gmail.com>
2023-08-02 19:40:20 +0300

    Made new role and some changes in the views for it

---
+++ Giftshop.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;namespace GiftShop.Web.Infrastructure.Extensions
+{
+    using System.Security.Claims;
+
+    using static GiftShop.Common.ApplicationConstants;
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string? GetId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRoleName);
+        }
+    }
+}
